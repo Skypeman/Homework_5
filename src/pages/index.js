ae2b27a83d3aa28269ddccf70538306b220e993e
Skypeman <kreoller@gmail.com>
2024-10-16 14:33:48 +0700

    add tests

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export * from './base.page.js';
+export * from './search.modal.js';
+export * from './nav.bar.js';
+export * from './app.page.js'
+export * from './search.page.js'
---
+++ src/pages/search.page.js
@@ -0,0 +1,30 @@
+import { BasePage } from "./base.page";
+import { step } from "allure-js-commons"
+
+class SearchPage extends BasePage {
+   constructor(page) {
+      super(page);
+      this.searchResultList = this.page.locator('div .result-item');
+      this.searchResultNamesList = this.page.locator('div .result__name');
+   }
+   async waitSearchResult() {
+      await step(`Ждём загрузку результатов поиска`, async () => {
+         await this.waitPageLoaded(this.searchResultList.first());
+      });
+   }
+
+   async getSearchResultNamesList() {
+      let nameList = [];
+      await step(`Заносим заголовки результатов поиска в массив`, async () => {
+         for await (const locator of await this.searchResultNamesList.all()) {
+            let name = await this.getElementText(locator);
+            nameList.push(name);
+         };
+      });
+      return nameList;
+   }
+
+
+
+}
+export { SearchPage }
